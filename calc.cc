abe934dc5b452f7c1390708541d5384b2ba52790
Orlando Diaz <100021501+Murdercows@users.noreply.github.com>
2022-04-23 17:47:39 -0400

    Update calc.cc

--- calc.cc
+++ calc.cc
@@ -4,12 +4,16 @@ using namespace std;
 
 void Menu() //Displays Menu
 {
-cout << "Press 1 for addition" << endl;
-cout << "Press 2 for subtraction" << endl;
-cout << "Press 3 for multipliction" << endl;
-cout << "Press 4 for division" << endl;
-cout << "Press 5 to exit" << endl;
+cout << "=============================" << endl;
+cout << "| Press 1 for addition      |" << endl;
+cout << "| Press 2 for subtraction   |" << endl;
+cout << "| Press 3 for multipliction |" << endl;
+cout << "| Press 4 for division      |" << endl;
+cout << "| Press 5 to exit           |" << endl;
+cout << "=============================" << endl;
 }
+
+
 
 void option1() //This will do addition :D
 {     
@@ -20,7 +24,7 @@ cin >> firstNum1;
 cout << "what is your second number" << endl;
 cin >> firstNum2;
 cout << "Your solution is: " << firstNum1 + firstNum2 << endl;
-cout << "Press 0 to go to Menu" << endl;
+cout << "Press any number to Return to Menu" << endl;
 }
 
 void option2() //This will do subtraction :D
@@ -32,7 +36,7 @@ cin >> secondNum1;
 cout << "what is your second number" << endl;
 cin >> secondNum2;
 cout << "Your solution is: " << secondNum1 - secondNum2 << endl;
-cout << "Press 0 to go to Menu" << endl;
+cout << "Press any number to Return to Menu" << endl;
 }
 
 void option3() //This will do multiplication :D
@@ -44,7 +48,7 @@ cin >> thirdNum1;
 cout << "what is your second number" << endl;
 cin >> thirdNum2;
 cout << "Your solution is: " << thirdNum1 * thirdNum2 << endl;
-cout << "Press 0 to go to Menu" << endl;
+cout << "Press any number to Return to Menu" << endl;
 }
 
 void option4() //This will do division :D
@@ -56,7 +60,7 @@ cin >> fourNum1;
 cout << "what is your second number" << endl;
 cin >> fourNum2;
 cout << "Your solution is: " << fourNum1 / fourNum2 << endl;
-cout << "Press 0 to go to Menu" << endl;
+cout << "Press any number to Return to Menu" << endl;
 }
 
 int main()
@@ -72,7 +76,7 @@ if (choice == 1)
 option1();
 int choice1;
 cin >> choice1;
-   if (choice1 == 0)
+   if (choice1 >= 0)
    return main();
 }
 
@@ -81,7 +85,7 @@ if (choice == 2)
 option2();
 int choice2;
 cin >> choice2;
-   if (choice2 == 0)
+   if (choice2 >= 0)
    return main();
 }
 
@@ -90,7 +94,7 @@ if (choice == 3)
 option3();   
 int choice3;
 cin >> choice3;
-   if (choice3 == 0)
+   if (choice3 >= 0)
    return main(); 
 }
 
@@ -99,15 +103,24 @@ if (choice == 4)
 option4(); 
 int choice4;
 cin >> choice4;
-   if (choice4 == 0)
+   if (choice4 >= 0)
    return main();   
 }
 
 if (choice == 5)
 {
-   cout << "Thank you for using Orlando's Calculator :D bye...";
+   
+   int choice5;
+   cout << "Thank you for using Orlando's Calculator :D bye..." << endl;
+   cout << "Press 5 again to exit" << endl;
+   cin >> choice5;
+   
+   if (choice5 == 5)
+   {
    return 0; 
+   }
 }
  
 return 0;
 }
+
